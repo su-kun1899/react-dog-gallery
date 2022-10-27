2158770bbb9f28a58a4e2fc5615acd85a197e63c
su-kun1899 <higedrum.coz@gmail.com>
2022-10-27 11:36:20 +0900

    fix: :white_check_mark: fix warning
    Warning: You are importing createRoot from "react-dom" which is not supported. You should instead import it from "react-dom/client".

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import "bulma/css/bulma.css";
 
-import {createRoot} from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from "./App";
 
 createRoot(document.querySelector("#content")).render(<App/>);
